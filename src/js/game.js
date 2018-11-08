7defe2a5d3e8275b0c882652264922484ab593aa
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-11-07 21:20:21 -0500

    evaluatenivel para todos los divs

--- src/js/game.js
+++ src/js/game.js
@@ -9,6 +9,7 @@ window.onload = () =>{
            userImage.innerHTML=`<img src="${user.photoURL}" width="90px" alt="user" class="profile-photo">`;
        }
        userUID = user.uid;
+       switchLevel();
    })
 }
 // termina de mostrarme la data del usuario y almacena el uid
@@ -26,61 +27,18 @@ document.getElementById('to1').addEventListener('click', ()=> {
         "nivel": 1
     })
     // deberiamos leer el nivel y avanzar
-    evaluateLevel('usuarios/'+userUID).then(level => {
-        switch (level) {
-            case 0:
-              //Sentencias ejecutadas cuando el resultado de expresion coincide con valor1
-              console.log('estas en el nivel 0');
-              break;
-            case 1:
-              //Sentencias ejecutadas cuando el resultado de expresion coincide con valor2
-              console.log('estas en el nivel 1');
-              level0.setAttribute('class', 'hiden')
-              level1.setAttribute('class','show')
-              break;
-            case 2:
-              //Sentencias ejecutadas cuando el resultado de expresion coincide con valorN
-              console.log('estas en el nivel 2')
-              break;
-            default:
-              //Sentencias_def ejecutadas cuando no ocurre una coincidencia con los anteriores casos
-              break;
-           }
-    })
+    switchLevel();
 })
 // PASAR DEL NIVEL 1 AL NIVEL 2
 document.getElementById('to2').addEventListener('click', ()=> {
     let sabChoosen = sabOptions.options[sabOptions.selectedIndex].value;
-    console.log(sabChoosen);
-    
     let userRef = firebase.database().ref('usuarios/'+userUID);
     // escribimos en la base de datos el nickname y aumentamos el nivel
     userRef.update({
         "sab" : sabChoosen,
         "nivel": 2
     })
-    // // deberiamos leer el nivel y avanzar
-    // evaluateLevel('usuarios/'+userUID).then(level => {
-    //     switch (level) {
-    //         case 0:
-    //           //Sentencias ejecutadas cuando el resultado de expresion coincide con valor1
-    //           console.log('estas en el nivel 0');
-    //           break;
-    //         case 1:
-    //           //Sentencias ejecutadas cuando el resultado de expresion coincide con valor2
-    //           console.log('estas en el nivel 1');
-    //           level0.setAttribute('class', 'hiden')
-    //           level1.setAttribute('class','show')
-    //           break;
-    //         case 2:
-    //           //Sentencias ejecutadas cuando el resultado de expresion coincide con valorN
-    //           console.log('estas en el nivel 2')
-    //           break;
-    //         default:
-    //           //Sentencias_def ejecutadas cuando no ocurre una coincidencia con los anteriores casos
-    //           break;
-    //        }
-    // })
+    switchLevel();
 })
 const evaluateLevel = (path) => {
     return new Promise((resolved, reject) => {
@@ -89,5 +47,38 @@ const evaluateLevel = (path) => {
             data = JSON.parse(data);
             resolved(data.nivel);
         })
+    })
+}
+const switchLevel = () => {
+    evaluateLevel('usuarios/'+userUID).then(level => {
+        switch (level) {
+            case 0:
+              //Sentencias ejecutadas cuando el resultado de expresion coincide con valor1
+              console.log('estas en el nivel 0');
+              level0.setAttribute('class', 'show')
+              level1.setAttribute('class','hiden')
+              level2.setAttribute('class', 'hiden')
+              level3.setAttribute('class','hiden')
+              break;
+            case 1:
+              //Sentencias ejecutadas cuando el resultado de expresion coincide con valor2
+              console.log('estas en el nivel 1');
+              level0.setAttribute('class', 'hiden')
+              level1.setAttribute('class','show')
+              level2.setAttribute('class', 'hiden')
+              level3.setAttribute('class','hiden')
+              break;
+            case 2:
+              //Sentencias ejecutadas cuando el resultado de expresion coincide con valorN
+              console.log('estas en el nivel 2')
+              level0.setAttribute('class', 'hiden')
+              level1.setAttribute('class','hiden')
+              level2.setAttribute('class', 'show')
+              level3.setAttribute('class','hiden')
+              break;
+            default:
+              //Sentencias_def ejecutadas cuando no ocurre una coincidencia con los anteriores casos
+              break;
+           }
     })
 }
